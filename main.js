6cc08d715c6de2350ad8b430bb0103d42716dbe2
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2021-01-12 20:44:21 -0600

    v-bind  v-if v-else  v-for v-on:click

--- main.js
+++ main.js
@@ -3,7 +3,19 @@
 const app = Vue.createApp({
     data(){
         return {
-            titulo: "hola mundo con vue.js"
+            titulo: "mi banco Vue.js",
+            cantidad : 0,
+            enlace : "https://youtube.com/isaicoding",
+            estado: true,
+            servicios : ["trasferencias", 
+                "pasgos", 
+                "giros",
+                "cheques"],
+        }
+    },
+    methods:{
+        agregarSaldo() {
+            this.cantidad = this.cantidad+100;
         }
     }
 })
